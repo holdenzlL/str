a8373da43313928b2ed0add9344765271a6da517
holdenzlL <94254770+holdenzlL@users.noreply.github.com>
2022-04-02 21:34:47 +0200

    almost ok

--- str/str/main.c
+++ str/str/main.c
@@ -1,47 +1,11 @@
-//#include <stdio.h>
-//#include "str.h"
 
-//int main()
-//{
-//    str_p p = readin_file_str("/Users/holden/Documents/GitHub/cstring/str/str/testfile");
-//    printf("hello");
-//    printf("%s", p->buffer);
-//    return 0;
-//
-//
-//}
-
-/* fread example: read an entire file */
 #include <stdio.h>
-#include <stdlib.h>
+#include "str.h"
 
-int main () {
-  FILE * pFile;
-  long lSize;
-  char * buffer;
-  size_t result;
-
-  pFile = fopen ( "/Users/holden/Documents/GitHub/cstring/str/str/testfile" , "r" );
-  if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
-
-  // obtain file size:
-  fseek (pFile , 0 , SEEK_END);
-  lSize = ftell (pFile);
-  rewind (pFile);
-
-  // allocate memory to contain the whole file:
-  buffer = (char*) malloc (sizeof(char)*lSize);
-  if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
-
-  // copy the file into the buffer:
-  result = fread (buffer,1,lSize,pFile);
-  if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
-    
-    printf("%s",buffer);
-  /* the whole file is now loaded in the memory buffer. */
-
-  // terminate
-  fclose (pFile);
-  free (buffer);
-  return 0;
+int main()
+{
+	str_p p = new_str(" world!");
+	write2_file_str("D:/Documents/GitHub/cstring/str/str/testfile", p);
+	printf("%s", readin_file_str("D:/Documents/GitHub/cstring/str/str/testfile")->buffer);
+	return 0;
 }
--- str/str/str.c
+++ str/str/str.c
@@ -4,11 +4,16 @@ str_p new_str(const char *string)
 {
     str_p p = (str_p)malloc(sizeof(str));
     if (!p)
+    {
+        perror("Error allocating memory");
         return NULL;
+    }
+ 
     p->size = sizeof_buff(string);
     p->buffer = (char *)malloc(p->size);
     if (!p->buffer)
     {
+        perror("Error allocating memory");
         p->size = 0;
         free(p);
         return NULL;
@@ -46,11 +51,16 @@ str_p zero_str(size_t size)
 {
     str_p p = (str_p)malloc(sizeof(str));
     if (!p)
+    {
+        perror("Error allocating memory");
         return NULL;
+    }
+        
     p->size = size;
     p->buffer = (char *)calloc(size, sizeof(char));
     if (!p->buffer)
     {
+        perror("Error allocating memory");
         p->size = 0;
         free(p);
         return NULL;
@@ -61,6 +71,8 @@ str_p zero_str(size_t size)
 str_p revs_str(const str_p p)
 {
     str_p q = zero_str(p->size);
+    if(!q)
+        return NULL;
     if (p->size)
     {
         for (size_t i = 0; i < p->size - 1; i++)
@@ -72,10 +84,9 @@ str_p revs_str(const str_p p)
     return q;
 }
 
-size_t sizeof_buff(const char *string)
+size_t sizeof_buff(const char* string)
 {
-    if (!string)
-        return 0;
+    //cant process NULL
     size_t i = 1;
     while (*string != '\0')
     {
@@ -101,7 +112,7 @@ void swp_str(str_p p, str_p q)
 
 int cmp_str(const str_p p, str_p q)
 {
-    const int n = memcmp(p->buffer, q->buffer, (p->size < q->size) ? p->size : q->size);
+    const int n = memcmp_str(p->buffer, q->buffer, (p->size < q->size) ? p->size : q->size);
 
     if (n != 0 || p->size == q->size)
         return n;
@@ -109,26 +120,161 @@ int cmp_str(const str_p p, str_p q)
     return (p->size < q->size) ? -1 : 1;
 }
 
-str_p readin_file_str(const char *filename)
+str_p readin_file_str(const char* filename)
 {
-    FILE *fp;
-    long size;
+    FILE* fp;
+    size_t size = 1;  //additional '\0'
 
-    fp = fopen(filename, "r");
-    if (!fp)
+    errno_t err = fopen_s(&fp, filename, "rb");
+    if (err)
     {
         perror("Error opening file");
         return NULL;
     }
+
     fseek(fp, 0, SEEK_END);
-    size = ftell(fp);
+    size += ftell(fp);
+    rewind(fp);
     str_p p = zero_str((size_t)size);
 
-    if (size != fread(p->buffer, 1, size, fp))
+    if (size - 1 != fread(p->buffer, sizeof(char), size, fp))
     {
         perror("Error reading file");
         free_str(p);
+        fclose(fp);
         return NULL;
+    }
+
+    p->buffer[size - 1] = '\0';
+
+    fclose(fp);
+    return p;
+}
+
+str_p append_str(str_p p, const char* string)
+{
+    size_t i = 0;
+    size_t size = sizeof_buff(string);
+    str_p new_p = (str_p)malloc(sizeof(str));
+    if (!new_p)
+    {
+        perror("Error allocating memory");
+        return NULL;
+    }
+        
+    new_p->size = size + p->size - 1;
+    new_p->buffer = (char*)malloc(new_p->size);
+    if (!p->buffer)
+    {
+        perror("Error allocating memory");
+        p->size = 0;
+        free(p);
+        return NULL;
+    }
+    for (i = 0; i < p->size - 1; i++)
+    {
+        new_p->buffer[i] = p->buffer[i];
+    }
+    for (i = 0; i < size; i++)
+    {
+        new_p->buffer[p->size - 1 + i] = string[i];
+    }
+    return new_p;
+}
+
+int memcmp_str(const char* s1, const char* s2, size_t n)
+{  
+    if (n != 0) {
+        register const unsigned char* p1 = s1, * p2 = s2;
+        do {
+            if (*p1++ != *p2++)
+                return (*--p1 - *--p2);
+        } while (--n != 0);
+    }
+    return (0);
+}
+
+str_p swap_chr(str_p p, unsigned long index1, unsigned long index2)
+{
+    if (index1 > p->size - 1 || index2 > p->size - 1)
+    {
+        perror("Error input");
+        return p;
+    }
+    char tmp = p->buffer[index1];
+    p->buffer[index1] = p->buffer[index2];
+    p->buffer[index2] = tmp;
+    return p;
+}
+
+size_t partition(char* arr, size_t low, size_t high)
+{
+    char pivot = arr[low];
+
+    for (;;) {
+        while (low < high && pivot <= arr[high]) {
+            high--;
+        }
+        if (low >= high) break;
+        arr[low++] = arr[high];
+
+        while (low < high && arr[low] <= pivot) {
+            low++;
+        }
+        if (low >= high) break;
+        arr[high--] = arr[low];
+    }
+
+    arr[high] = pivot;
+    return high;
+}
+
+void quickSort(char* arr, size_t low, size_t high)
+{
+    size_t pi;
+
+    if (low < high)
+    {
+        pi = partition(arr, low, high);
+        if (pi)
+        {
+            quickSort(arr, low, (pi - 1));
+        }
+        quickSort(arr, (pi + 1), high);
+    }
+}
+
+str_p nsort_str(str_p p, size_t low, size_t high)
+{
+    if (low < 0 || high > p->size)
+    {
+        perror("Invalid index");
+        return p;
+    }
+    quickSort(p->buffer, low, high);
+    return p;
+}
+
+str_p sort_str(str_p p)
+{
+    quickSort(p->buffer, 0, p->size - 2);
+    return p;
+}
+
+void write2_file_str(const char* filename, const str_p p)
+{
+    FILE* fp;
+    errno_t err = fopen_s(&fp, filename, "ab+");
+    if (err)
+    {
+        perror("Error opening file");
+        return;
+    }
+    if (p->size - 1 != fwrite(p->buffer, sizeof(char), p->size - 1, fp))
+    {
+        perror("Error writing file");
+        fclose(fp);
+        return;
     }
     fclose(fp);
     return p;
